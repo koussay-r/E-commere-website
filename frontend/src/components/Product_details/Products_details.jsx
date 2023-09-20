59d4ef848e3fbf93c4458931aba3f0330288c146
koussay-r <koussayr98@gmail.com>
2023-09-20 17:11:54 +0100

    done with the product details page

--- frontend/src/components/Product_details/Products_details.jsx
+++ frontend/src/components/Product_details/Products_details.jsx
@@ -16,8 +16,28 @@ export default function Products_details() {
         handleGetDataForProductPicked()
         },[])
   return (
-    <div>
-        <img src={ProductData.image}/>
+    <div className='flex border border-transparent border-b shadow  flex-row '>
+        <img src={ProductData.image} className='md:h-[650px] h-[400px]  w-full  object-cover  md:w-[35%]'/>
+        <div className=' w-[55%] ml-20 mt-28'>
+            <p className='text-[60px]'>{ProductData.name}</p>
+            <p className='text-[30px]'>${ProductData.price}.00</p>
+            <p className='mt-10'>{ProductData.discription}</p>
+            <div className='block w-full m-auto'>
+                <p className='text-[20px] mt-10 font-semibold'>Size</p>
+                <select className='w-full bg-gradient-to-b from-white to-gray-200 mt-5 pl-4 py-2 '>
+                    <option selected>Select size</option>
+                    <option>XS</option>
+                    <option>S</option>
+                    <option>M</option>
+                    <option>L</option>
+                    <option>XL</option>
+                    <option>XLL</option>
+                </select>
+                <p className='mt-4'>Quantity</p>
+                <input type={"number"} min={"1"} max={"100"} className='outline-none border border-gray-400 mt-2 w-[60px] py-1 px-2' placeholder='1' />
+            </div> 
+            <button className='mt-4 bg-black/80 text-white px-5 py-3'>Add to chart</button>
+        </div>
     </div>
   )
 }
